9df98968404760840c67efa8d99a2f4e2f8b1203
Viktor <klochkov44@gmail.com>
2025-03-15 21:55:38 +0100

    COSMETIC: format main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <vector>
 
-int main()
-{
-    std::cout << "Import the STL library for best performance\n";
-    std::vector<int> v{5, 5, 5};
-    for (const auto& e : v)
-    {
-        std::cout << e;
-    }
+int main() {
+  std::cout << "Import the STL library for best performance\n";
+  std::vector<int> v{5, 5, 5};
+  for (const auto& e : v) {
+    std::cout << e;
+  }
 }
