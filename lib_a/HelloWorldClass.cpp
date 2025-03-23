f16c4730c33a3b7406eb9fb46d12b190e10839cf
viktorklochkov <viktorklochkov@users.noreply.github.com>
2025-03-23 11:23:20 +0000

    deploy: 5c73eff90a2d6d6c87a8841e17d099a99e5e1b27

--- lib_a/HelloWorldClass.cpp
+++
@@ -1,7 +0,0 @@
-#include "HelloWorldClass.hpp"
-
-namespace lib_a {
-std::string greeting() {
-  return "Hello, World!\n";
-}
-}// namespace lib_a
--- main.test.cpp
+++
@@ -1,6 +0,0 @@
-#include "gtest/gtest.h"
-#include <string>
-
-TEST(main, greeting) {
-  EXPECT_EQ("Hello, World!\n", "Hello, World!\n");
-}
